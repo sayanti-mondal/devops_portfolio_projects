bc1049abd8c45bea513daf9732dee3b33a309e86
sayanti-mondal <sayantiju2014@gmail.com>
2025-08-04 04:58:46 +0000

    commit6

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -49,6 +49,21 @@ resource "aws_instance" "ansible_target" {
               yum install -y python3 # For Amazon Linux 2 / CentOS
               # For Ubuntu, use: apt update -y && apt install -y python3
               echo "Python installed for Ansible."
+
+              # Add Docker's official GPG key:
+              sudo apt-get update
+              sudo apt-get install ca-certificates curl
+              sudo install -m 0755 -d /etc/apt/keyrings
+              sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+              sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+              # Add the repository to Apt sources:
+              echo \
+                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+                $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update
+              
               EOF
 
   tags = {
